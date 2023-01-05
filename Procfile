04935b3ae6dcb5acc5368b5856ab79f05b8cd738
user2304TK <kietvampire2@gmail.com>
2023-01-05 13:48:31 +0700

    1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'Shop_Teki.wsgi'
---
+++ store/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Product, Variation, ReviewRating
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock', 'category', 'created_date', 'modified_date', 'is_available')
+    prepopulated_fields = {'slug': ('product_name',)}
+
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active', 'created_date')
+    list_editable = ('is_active',)   # Cho phép chỉnh sửa trên list hiển thị
+    list_filter = ('product', 'variation_category', 'variation_value')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
+admin.site.register(ReviewRating)
